47300548228c0a2198b7b7b7c23134de6da9d4f0
Your Name <fahrisyany@gmail.com>
2018-05-29 20:19:55 +0700

    challange finish

--- linear_search.js
+++ linear_search.js
@@ -2,12 +2,27 @@
 
 // Release 0
 console.log("Linear Search")
+
 let linearSearch = (target, values) => {
   //write your code here
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+
+
+      return i
+
+
+    }
+
+
+  }
+
+  return -1
+
+
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
-
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 console.log(linearSearch(18, random_numbers));
 // 2
 console.log(linearSearch(9, random_numbers));
@@ -17,6 +32,19 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var arr = []
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] == target) {
+
+      arr.push(i)
+
+    }
+
+
+  }
+
+  return arr
+
 }
 
 let banana_arr = "banana".split("");
