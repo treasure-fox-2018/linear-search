2e77734632c4e48f0b1348542c75f4b44f23706c
ronifajrian <ronifajrian@gmail.com>
2018-05-29 16:58:55 +0700

    add linear_search

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (let i = 0; i < random_numbers.length; i++) {
+    if (target == random_numbers[i]) {
+      return i
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +23,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var arrFind = []
+  for (let i = 0; i < banana_arr.length; i++) {
+    if (target == banana_arr[i]) {
+      arrFind.push(i)
+    }
+  }
+  return arrFind
 }
 
 let banana_arr = "banana".split("");
