95a10b425d9f8adad72e904239942f24a1f0af2b
sumarsanaadi <sumarsana.adi@gmail.com>
2018-05-29 19:49:10 +0700

    linear search 1st commit sumarsanaadi

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,12 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (let i = 0 ; i < values.length ; i++) {
+    if (target === values[i]) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,13 +21,23 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let output = [];
+  for (let i = 0 ; i < values.length ; i++) {
+    if (target === values[i]) {
+      output.push(i);
+    }
+  }
+  if (output.length === 0) {
+    return -1;
+  } else return output;
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
+console.log(globalLinearSearch("Z", banana_arr));
+// -1
 
 module.exports = {
   linearSearch,
