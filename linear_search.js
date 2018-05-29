a31900c5f5c434a62f6f83aa5fdb50983a0eebfc
Wahyudi Setiaji <wahyudisetiaji@gmail.com>
2018-05-29 23:33:05 +0700

    ADD linear search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,18 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
+let countTarget = 0;
   //write your code here
+  for(var i = 0; i < values.length; i++){
+    if(target === values[i]){
+      countTarget++
+    }
+  }
+  if(countTarget === 0){
+    return countTarget - 1
+  }else {
+    return countTarget;
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +28,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let arrTarget = [];
+  for(var i = 0; i < values.length; i++){
+    if(target === values[i]){
+      arrTarget.push(i)
+    }
+  }
+  return arrTarget
 }
 
 let banana_arr = "banana".split("");
